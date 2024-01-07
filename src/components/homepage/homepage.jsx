f3ce1ba9e71253d8e63a170b349e32360cd90e0e
rahulrelekar <citzrahul@gmail.com>
2024-01-07 13:59:07 +0530

    added comment in homepage component

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -3,6 +3,7 @@ import "./homepage.css"
 
 const Homepage = ({setLoginUser, user, logout}) => {
     return (
+        // test1
         <div className="homepage">
             <h1>Welcome {user.name} to Kimshuka Technologies</h1>
             <div className="button" onClick={logout}>Logout</div>
